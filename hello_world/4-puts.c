1f19da214d6707723d8714eb701d2a8bb52bf3bf
14822-Ab-L <14822@holbertonstudents.com>
2026-07-11 21:03:44 +0000

    Add C program using puts to print text

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main - Prints a string using the function puts
+ *
+ * Return: Always end with the value 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
