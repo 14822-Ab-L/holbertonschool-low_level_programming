323b98ff121d6de88876fadd7647b3153658f346
14822-Ab-L <14822@holbertonstudents.com>
2026-07-12 06:38:55 +0000

    Add C program to print lowercase and uppercase alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase and uppercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
