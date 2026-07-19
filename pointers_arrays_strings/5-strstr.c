becb20963938f4d84ecfd0dc2d8423f42d067397
14822-Ab-L <14822@holbertonstudents.com>
2026-07-19 13:39:17 +0000

    Add _strstr function

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring in a string
+ * @haystack: string to search
+ * @needle: substring to find
+ *
+ * Return: pointer to beginning of substring, or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
