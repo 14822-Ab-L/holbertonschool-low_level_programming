531afe38401f37ee0bc5b98e40a9ee61cdef1ac2
14822-Ab-L <14822@holbertonstudents.com>
2026-08-02 13:13:32 +0000

    Add calculator program using function pointers

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stddef.h>
 
 /**
  * struct op - Struct op
@@ -22,6 +23,5 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
-
 
 #endif
