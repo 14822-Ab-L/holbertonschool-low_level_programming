723f6430a1fdaff875fc62336b81672eac501d76
14822-Ab-L <14822@holbertonstudents.com>
2026-08-02 12:53:22 +0000

    Add int_index function

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search
+ * @size: size of array
+ * @cmp: function used to compare values
+ *
+ * Return: index of matching element, or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	i = 0;
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+
+		i++;
+	}
+
+	return (-1);
+}
