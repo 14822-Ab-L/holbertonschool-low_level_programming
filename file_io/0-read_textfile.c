8d7cbc0ce7c4f3d1d6ac4bf67ebe75968994da9e
14822-Ab-L <14822@holbertonstudents.com>
2026-08-21 12:13:46 +0000

    Add read textfile function

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: name of the file to read
+ * @letters: number of bytes to read and print
+ *
+ * Return: number of bytes read and printed, or 0 if an error occurs
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd;
+	ssize_t wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	rd = read(fd, buffer, letters);
+	if (rd == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr != rd || wr == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (wr);
+}
